04cb0542ba654176a50a5ddb5d7c7905416e08cb
Lucy Barber <lbarber729@hotmail.co.uk>
2019-04-23 11:11:49 +0100

    commit with methods

--- directory.rb
+++ directory.rb
@@ -12,11 +12,18 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-#and then print them
+def print_header
 puts "The students of Villains Academt"
 puts "-------------"
-students.each do |student|
-  puts student
-end 
-#finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+end
+def print(names)
+names.each do |name|
+  puts name
+  end 
+end
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+print_header
+print(students)
+print_footer(students)
