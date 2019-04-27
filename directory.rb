cc474afe2094cb606c32508c3acf5a2c42d07640
Lucy Barber <lbarber729@hotmail.co.uk>
2019-04-27 19:04:11 +0100

    final upload

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 @students = [] #an empty array accessible to all methods
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split (',')
     @students << {name: name, cohort: cohort.to_sym}
@@ -9,18 +9,30 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first #first argument from tthe command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exists?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit #quit the program
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "to finish, just hit return twice"
   #get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -39,7 +51,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -90,4 +102,5 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
+try_load_students
 interactive_menu
